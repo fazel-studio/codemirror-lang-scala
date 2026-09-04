f019ed9fbfd1d00d839d123d59346f167c104771
Zulfazli <zulfazlilsm@gmail.com>
2026-09-04 23:09:16 +0700

    chore: remove unused parser files and test scripts

--- parse_test_indent.js
+++
@@ -1,6 +0,0 @@
-import { parser } from "./src/parser.js";
-import fs from "fs";
-
-const code = fs.readFileSync("../../Notron/test/test_indent.scala", "utf-8");
-const tree = parser.parse(code);
-console.log(tree.toString());
